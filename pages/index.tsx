d4fb045adf79933f7e1781f3a966b44736c12207
danielleond99 <87273355+danielleond99@users.noreply.github.com>
2023-11-29 12:08:20 -0500

    config nextui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import { Button } from "@nextui-org/react";
 
 const Home: NextPage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button
+        radius="full"
+        className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+      >
+        Button
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
